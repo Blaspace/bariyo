9962030f5278b8285f2d2f8903028167c18dc8b0
unknown <js5618171@gmail.com>
2025-11-08 16:45:25 +0100

    hello

--- src/pages/EmailVerification.js
+++ src/pages/EmailVerification.js
@@ -25,7 +25,7 @@ function Verify() {
       })
         .then((res) => {
           if (res.ok) {
-            res.json();
+           return res.json();
           } else if (res.status === 401) {
             setMessage(
               "the verification code you sent was invalid please try agein"
@@ -33,8 +33,6 @@ function Verify() {
           }
         })
         .then((data) => {
-          console.log(data);
-          
           localStorage.setItem("token", JSON.stringify(data.token));
           navigate("/");
         })
